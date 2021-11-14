7c3255d7637ecb0f1189c45c7570ac0f5a0610b4
Nikita Shalimov <nikitashalimov@mail.ru>
2021-11-14 19:12:20 +0300

    2 commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 export default class Team {
-	constructor() {
-		this.members = new Set();
-	}
+  constructor() {
+    this.members = new Set();
+  }
 
-	add(Character) {
-		if (this.members.has(Character)) {
-			throw new Error('Such a character is already available in this team');
-		}
-		this.members.add(Character);
-	}
+  add(Character) {
+    if (this.members.has(Character)) {
+      throw new Error('Such a character is already available in this team');
+    }
+    this.members.add(Character);
+  }
 
-	addAll(Characters) {
-		Characters.forEach((element) => {
-			this.members.add(element);
-		});
-	}
+  addAll(Characters) {
+    Characters.forEach((element) => {
+      this.members.add(element);
+    });
+  }
 
-	toArray() {
-		return Array.from(this.members);
-	}
+  toArray() {
+    return Array.from(this.members);
+  }
 }
